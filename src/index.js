980b109878e5b53f65e32ce9970457919e226c2a
vaishnavidumbare05 <vaishnavidumbare2004@gmail.com>
2024-10-02 07:12:35 +0530

    Common asset of quick note application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import{ createBrowserRouter,RouterProvider} from "react-router-dom"
+import Home from './Views/Home/Home';
+import Show from './Views/Show/Show';
+import Add from './Views/Add/Add';
+import {Toaster} from "react-hot-toast"
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Home/>
+  },
+  {
+    path:"/add",
+    element:<Add/>
+  },
+  {
+    path:"/show",
+    element:<Show/>
+  },
+  {
+    path:"*",
+    element:<h2>404 not found</h2>
+  }
+])
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+<RouterProvider router={router}/>
+<Toaster/>
+  </div>
+  
+ 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
